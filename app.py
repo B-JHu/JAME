1fb655b2d8bcf01953d93781a459cf37adbf906b
B-JHu <sesam120063@gmail.com>
2022-08-17 15:36:33 +0200

    Implement missing menubar functions
    Some buttons from the menubar - specifically the "Quit" option and the entire "Help" section - did not have a function associated with them and hence would not do anything when clicked. This has now been fixed.
    Also did some minor renaming/rewording of some stuff in app.py & layout.py

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from functools import partial
 import sys
 import os
 import re
-from PyQt5 import QtWidgets, QtGui
+from PyQt5 import QtWidgets, QtGui, QtCore
 
 from libs import parser, htmlrenderer, jsonrenderer
 from layout import Ui_mainWindow
+
+__version__ = "0.1.0"
 
 class MainWindow(QtWidgets.QMainWindow, Ui_mainWindow):
     def __init__(self, *args, obj=None, **kwargs):
@@ -30,6 +32,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_mainWindow):
         self.actionSave_as.triggered.connect(self.menubar_save_as_clicked)
         self.actionNew.triggered.connect(self.menubar_new_clicked)
         self.actionOpen.triggered.connect(self.menubar_open_clicked)
+        self.actionQuit.triggered.connect(self.menubar_quit_clicked)
 
         self.actionHTML_plain.triggered.connect(self.menubar_export_to_HTML_plain_clicked)
         self.actionHTML_styled.triggered.connect(self.menubar_export_to_HTML_styled_clicked)
@@ -48,6 +51,10 @@ class MainWindow(QtWidgets.QMainWindow, Ui_mainWindow):
         self.actionHeading_Lvl_1.triggered.connect(partial(self.insertMarkdown, what_to_insert="Heading 1"))
         self.actionHeading_Lvl_2.triggered.connect(partial(self.insertMarkdown, what_to_insert="Heading 2"))
         self.actionHeading_Lvl_3.triggered.connect(partial(self.insertMarkdown, what_to_insert="Heading 3"))
+
+        self.actionGitHub_open_repo.triggered.connect(self.menubar_GitHub_repo_clicked)
+        self.actionMarkdown_Tutorial.triggered.connect(self.menubar_Markdown_tutorial_clicked)
+        self.actionAbout_JAME.triggered.connect(self.menubar_about_clicked)
 
         # correct the relative sizes of the elements in the splitter
         self.splitter.setStretchFactor(0, 1)
@@ -129,6 +136,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_mainWindow):
 
     def menubar_save_as_clicked(self):
         file_name_to_save_to, filter_chosen = QtWidgets.QFileDialog.getSaveFileName(self, "Save your work", os.getcwd() + "/untitled", "Markdown files (*.md)")
+        if not file_name_to_save_to: # User has chosen the "Cancel" option
+            return
+
         if not re.search(r'\.md$', file_name_to_save_to):
             file_name_to_save_to += ".md"
 
@@ -169,7 +179,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_mainWindow):
             self.current_open_file = file_name_to_open
             self.setWindowTitle(f"JAME: just another Markdown editor — {self.current_open_file}")
         else:
-            save_dialog = QtWidgets.QMessageBox(QtWidgets.QMessageBox.Icon.Question, "Save or discard changes", "Do you want to save the current document before opening a new one?", QtWidgets.QMessageBox.StandardButton.Save | QtWidgets.QMessageBox.StandardButton.Discard | QtWidgets.QMessageBox.StandardButton.Cancel)
+            save_dialog = QtWidgets.QMessageBox(QtWidgets.QMessageBox.Icon.Question, "Save or discard changes", "Do you want to save the current document before opening another one?", QtWidgets.QMessageBox.StandardButton.Save | QtWidgets.QMessageBox.StandardButton.Discard | QtWidgets.QMessageBox.StandardButton.Cancel)
             chosen_option = save_dialog.exec()
 
             match chosen_option:
@@ -197,6 +207,34 @@ class MainWindow(QtWidgets.QMainWindow, Ui_mainWindow):
                     
                 case QtWidgets.QMessageBox.StandardButton.Cancel:
                     pass
+
+    def menubar_quit_clicked(self):
+        if len(self.text) == 0:
+            sys.exit()
+        else:
+            save_dialog = QtWidgets.QMessageBox(QtWidgets.QMessageBox.Icon.Question, "Save or discard changes", "Do you want to save the current document before quitting?", QtWidgets.QMessageBox.StandardButton.Save | QtWidgets.QMessageBox.StandardButton.Discard | QtWidgets.QMessageBox.StandardButton.Cancel)
+            chosen_option = save_dialog.exec()
+
+            match chosen_option:
+                case QtWidgets.QMessageBox.StandardButton.Save:
+                    self.menubar_save_clicked()
+                    sys.exit()
+
+                case QtWidgets.QMessageBox.StandardButton.Discard:
+                    sys.exit()
+
+                case QtWidgets.QMessageBox.StandardButton.Cancel:
+                    pass
+
+    def menubar_GitHub_repo_clicked(self):
+        QtGui.QDesktopServices.openUrl(QtCore.QUrl("https://github.com/B-JHu/JAME/"))
+
+    def menubar_Markdown_tutorial_clicked(self):
+        QtGui.QDesktopServices.openUrl(QtCore.QUrl("https://commonmark.org/help/tutorial/"))
+
+    def menubar_about_clicked(self):
+        about_string_with_version = f"JAME - just another Markdown editor\nVersion {__version__}\n\nPublished under the GPL v3 license"
+        QtWidgets.QMessageBox.about(self, f"About JAME", about_string_with_version)
 
     # exporters
     def menubar_export_to_HTML_plain_clicked(self):
